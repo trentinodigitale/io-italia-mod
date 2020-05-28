f8fc803d6ac4a1a6e2c1a1f8c900982d59414b0d
kazhamiakin <kazhamiakin@gmail.com>
2020-05-28 08:57:35 +0200

    OAuth2.0 integration

--- iot-send/src/main/java/it/tndigit/iot/IOTSendApp.java
+++ iot-send/src/main/java/it/tndigit/iot/IOTSendApp.java
@@ -3,14 +3,11 @@ package it.tndigit.iot;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,14 +20,6 @@ public class IOTSendApp {
 
     public static void main(String[] args) {
         SpringApplication.run(IOTSendApp.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        registration.setOrder(-100);
-        return registration;
     }
 
     @Bean
